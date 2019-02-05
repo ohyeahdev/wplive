1965d250e2963d13cab7836728eabb79de5bbabe
Oscar Abad Folgueira <oscar@dinapyme.com>
2019-02-05 12:25:03 +0100

    Subida de fichero con descripción y código

---
+++ 001/wplive001-como-aniadir-codigo-a-wordpress.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * #develotips001- Primer programa de WPLIVE
+ * Author: Oscar Abad Folgueira
+ * Author URL: https://ohyeahdev.com
+ * Título: Cómo añadir código a WordPress
+ */ 
+
+/*
+ * Descripción
+ *
+ * Diferentes formas de añadir código a WordPress, de mejor a peor:
+ * 
+ * 1-. Crear un mu-plugin de funciones.
+ * 2-. Crear un plugin de funciones.
+ * 3-. Instalar el plugin "Code Snippets".
+ * 4-. Modificar el fichero "functions.php" del tema (desde nuestro ordenador).
+ * 5-. Modificar el fichero "functions.php" desde la administración de WordPress.
+ *
+ */
+
+/* 
+ * Snippet utilizado:  Quitar la barra de administración en el front
+ */
+
+add_filter( 'show_admin_bar', '__return_false' );
